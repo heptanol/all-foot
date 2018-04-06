a59e3e55cd5402459e7b7d265dc10976867360e6
heptanol <thamine.alaeddine@gmail.com>
2018-04-07 01:25:14 +0200

    clean code

--- src/app/shared/foot-api.service.ts
+++ src/app/shared/foot-api.service.ts
@@ -17,32 +17,8 @@ export class FootApiService {
     return this.http.get(`${this.apiUrl}/competitions/${league}/fixtures?matchday=${matchday}`);
   }
 
-  getFixturesBeta(league, matchday): Observable<any> {
-    return this.http.get(`${this.apiUrl}/competitions/${league}/fixtures/?matchday=${matchday}`)
-      .pipe(
-        mergeMap(data => data['fixtures']),
-        map(fixture => ({
-          id: fixture['id'],
-          awayTeamId: fixture['awayTeamId'],
-          awayTeamName: fixture['awayTeamName'],
-          homeTeamId: fixture['homeTeamId'],
-          homeTeamName: fixture['homeTeamName'],
-          matchday: fixture['matchday'],
-          date: fixture['date'],
-          status: fixture['status'],
-          result: fixture['result'],
-          day: this.getDay(fixture['date'])
-        })),
-        toArray()
-      );
-  }
-
   getCompetition(league): Observable<Competition> {
     return this.http.get<Competition>(`${this.apiUrl}/competitions/${league}`);
-  }
-
-  getCompetitionAlfa(league): Observable<Competition> {
-    return this.http.get<Competition>(`http://api.football-data.org/v1/competitions/${league}`);
   }
 
   getCompetitionTable(league): Observable<Ranking> {
@@ -52,11 +28,5 @@ export class FootApiService {
 
   getCompetitions(): Observable<any> {
     return this.http.get(`${this.apiUrl}/competitions`);
-  }
-
-  private getDay(date) {
-    const dt = new Date(date);
-    dt.setHours(0, 0, 0, 0);
-    return dt.toISOString();
   }
 }
