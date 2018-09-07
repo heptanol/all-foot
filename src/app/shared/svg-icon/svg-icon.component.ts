6249e94c003d9477fbb9e60d6b9c412013f079cc
heptanol <thamine.alaeddine@gmail.com>
2018-09-08 01:42:12 +0200

    team logo component integration

--- src/app/shared/svg-icon/svg-icon.component.ts
+++ src/app/shared/svg-icon/svg-icon.component.ts
@@ -2,7 +2,8 @@ import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-svg-icon',
-  templateUrl: './svg-icon.component.html'
+  templateUrl: './svg-icon.component.html',
+  styleUrls: ['./svg-icon.component.scss']
 })
 export class SvgIconComponent {
   @Input() id: String;
