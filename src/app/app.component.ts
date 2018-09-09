ff5088522e01429f8989306e0aabc880d38ac4ca
heptanol <thamine.alaeddine@gmail.com>
2018-09-09 22:31:44 +0200

    Add title and meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,15 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  constructor(meta: Meta, title: Title) {
+    title.setTitle('Footpaper');
+
+    meta.addTags([
+      { name: 'author',   content: 'Footpaper.com'},
+      { name: 'keywords', content: 'Footpaper, Football, foot'},
+      { name: 'description', content: '' }
+    ]);
+  }
 
   ngOnInit() {
   }
