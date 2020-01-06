4fb5f18dfc1dbbe0bd178b48c227af3284c47097
Alaeddine Thamine <alaeddine.thamine.prestaire@sfr.com>
2020-01-06 09:42:59 +0100

    Upgrade to Angular 6.1

--- src/app/shared/foot-api.service.ts
+++ src/app/shared/foot-api.service.ts
@@ -1,9 +1,8 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
-import {Observable} from 'rxjs/Observable';
-import {environment} from '../../environments/environment';
-import { map } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 import { CompetitionResponse } from './model';
+import { Observable } from 'rxjs/Observable';
 
 
 @Injectable()
