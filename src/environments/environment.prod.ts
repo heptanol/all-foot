90235287c9982be7fcc44b211e4cc9bfea3e89da
heptanol <thamine.alaeddine@gmail.com>
2020-05-12 12:40:13 +0200

    config docker prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://api.footpaper.info/public',
+  apiUrl: 'http://footpaper.info:8080/public',
   google_analytics_code: 'UA-54501284-4'
 };
